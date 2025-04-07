da60376e59685c2c0e7d22f6b43ed03919852007
Lucas Henrique <166299365+Lucas3255@users.noreply.github.com>
2025-04-07 08:40:15 -0300

    atualização sistema

--- ArvoreDeNatal/arvore_de_natal.c
+++ ArvoreDeNatal/arvore_de_natal.c
@@ -1,36 +1,73 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <Windows.h>
+#include <time.h>
+
+void carregar();
 
 int main ()
 {
 	int i;		//contador de quebra de linha
 	int j;		//contador das bolinhas
 	int a;		//contador para o espaçamento
-	int tamanho, tronco;
+	int tamanho, tronco, escolha;
 		
-	printf("qual o tamanho a arvore vai ter? \n >> ");
-	scanf("%d", &tamanho);
-	system("cls");
-	printf("\n");
-	tronco = tamanho / 2;
-
-	for (i = 0; i <= tamanho - 1; i++) {
-		for (a = tamanho; a >= i; a--){
+	do {
+		printf(" Qual o tamanho a arvore vai ter? \n >> ");
+		scanf("%d", &tamanho);
+		fflush(stdin);
+		system("cls");
+		printf("\n");
+		tronco = tamanho / 2;
+	
+		for (i = 0; i <= tamanho - 1; i++) {
+			for (a = tamanho; a >= i; a--){
+				printf(" ");
+			}
+			
+			for (j = 0; j <= i * 2; j++) {
+				printf("*");
+			}
+			printf("\n");
+		}
+		
+		for (a = 0; a <= tamanho-1; a++){
 			printf(" ");
 		}
-			
-		for (j = 0; j <= i * 2; j++) {
-			printf("*");
+	
+		for (j = 0; j <= tronco; j++){
+			printf("|");
 		}
-		printf("\n");
-	}
-	
-	for (a = 0; a <= tamanho-1; a++){
-		printf(" ");
-	}
-	
-	for (j = 0; j <= tronco; j++){
-		printf("|");
-	}
+		sleep(3);
+		
+		do {
+			printf("\n\n-------------------------\nDeseja fazer de novo?");
+			printf("\n 1 - Sim\n 2 - Nao\n\n >> ");
+			scanf("%d", &escolha);
+			fflush(stdin);
+			system("cls");
+			
+			if (escolha == 2){
+				system("cls");
+				printf(" Encerrando Programa...\n");
+				carregar();
+				return 0;
+			} 
+			if (escolha != 1 || escolha != 2){
+				system("cls");
+				printf(" A opcao %d eh invalida.\n", escolha);
+				sleep(3);
+				system("pause");
+				system("cls");
+			}
+		}while(escolha != 1, 2);
+		
+	}while(escolha == 1);
+}
 
+void carregar(){
+	int i;
+	for (i = 0; i <= 2; i++) {
+		printf(" o ");
+		sleep(1);
+	}
 }
